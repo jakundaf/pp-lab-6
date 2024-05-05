3d3e1f580f7add068c2ba50e2115fcf6050ac866
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-05 18:49:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,60 @@
 import company.models.*;
 import company.abstracts.Employee;
 
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Johny", 469469, 1, "01-01-1990", "worker");
         Worker worker2 = new Worker("Bravo", 486486, 2, "02-02-1991", "worker");
         Worker worker3 = new Worker("Mahatma", 3478, 2, "03-03-1992", "worker");
+        Worker worker4 = new Worker("Gandhi", 15000, 4, "04-04-1993", "worker");
 
         Manager manager = new Manager("Bruce", 60000, 5, "05-05-1994", "manager");
+        Manager manager1 = new Manager("Lee", 45000, 2, "06-06-1995", "manager");
 
-        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
-        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
-        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
-        System.out.println(manager.getName() + " has code: " + manager.hashCode());
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+        employees.add(manager1);
 
-        System.out.println("Worker2 (" + worker2.getName() + ") equals worker1 (" + worker1.getName() + "): " + worker2.equals(worker1));
-        System.out.println("Worker2 (" + worker2.getName() + ") equals worker3 (" + worker3.getName() + "): " + worker2.equals(worker3));
-        System.out.println("Worker2 (" + worker2.getName() + ") equals manager (" + manager.getName() + "): " + worker2.equals(manager));
+        double totalSalary = 0;
+        double totalManagerSalary = 0;
+        double totalWorkerSalary = 0;
+        Map<Integer, ArrayList<Employee>> map = new HashMap<>();
 
+        for (Employee employee : employees){
+            totalSalary += employee.getSalary();
+            if (employee instanceof Manager){
+                totalManagerSalary += employee.getSalary();
+            } else if (employee instanceof Worker){
+                totalWorkerSalary += employee.getSalary();
+            }
+            if (map.containsKey(employee.hashCode())) {
+                map.get(employee.hashCode()).add(employee);
+            } else {
+                ArrayList<Employee> employeeList = new ArrayList<>();
+                employeeList.add(employee);
+                map.put(employee.hashCode(), employeeList);
+            }
+        }
+
+        System.out.println("Total salary: " + totalSalary);
+        System.out.println("Total manager salary: " + totalManagerSalary);
+        System.out.println("Total worker salary: " + totalWorkerSalary);
+
+        for (Map.Entry<Integer, ArrayList<Employee>>  entry : map.entrySet()) {
+            ArrayList<Employee> employeeList = entry.getValue();
+            if (employeeList.size() > 1) {
+                System.out.println("Employyes with same ID: ");
+                for (Employee employee : employeeList){
+                    System.out.println(employee.getName());
+                }
+            }
+        }
     }
 }
