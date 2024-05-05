244fd5a3e880867a978b6b521f2abe6147c532cf
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-05 17:53:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,28 @@
 import company.models.*;
+import company.abstracts.Employee;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Johny", 469469, 1);
-        Worker worker2 = new Worker("Bravo", 486486, 2);
-        Worker worker3 = new Worker("Mahatma", 3478, 3);
-        Worker worker4 = new Worker("Gandhi", 15000, 4);
+        Worker worker1 = new Worker("Johny", 469469, 1, "01-01-1990", "worker");
+        Worker worker2 = new Worker("Bravo", 486486, 2, "02-02-1991", "worker");
+        Worker worker3 = new Worker("Mahatma", 3478, 3, "03-03-1992", "worker");
+        Worker worker4 = new Worker("Gandhi", 15000, 4, "04-04-1993", "worker");
 
-        Manager manager = new Manager("Bruce", 60000, 5);
+        Manager manager = new Manager("Bruce", 60000, 5, "05-05-1994", "manager");
 
-        System.out.println("Salary of worker1: " + worker1.getSalary());
-        worker1.work();
-        System.out.println("Salary of worker2: " + worker2.getSalary());
-        worker2.work();
-        System.out.println("Salary of worker3: " + worker3.getSalary());
-        worker3.work();
-        System.out.println("Salary of worker4: " + worker4.getSalary());
-        worker4.work();
-        System.out.println("Salary of manager: " + manager.getSalary());
-        manager.work();
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+
+        for (Employee employee : employees){
+            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " +
+                    employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary());
+        }
+
     }
 }
