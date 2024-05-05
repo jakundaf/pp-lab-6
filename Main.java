104c2487cb10e1b880b2cbc3dffa60caf7ae9790
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-05-05 18:12:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,22 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Johny", 469469, 1, "01-01-1990", "worker");
         Worker worker2 = new Worker("Bravo", 486486, 2, "02-02-1991", "worker");
-        Worker worker3 = new Worker("Mahatma", 3478, 3, "03-03-1992", "worker");
-        Worker worker4 = new Worker("Gandhi", 15000, 4, "04-04-1993", "worker");
+        Worker worker3 = new Worker("Mahatma", 3478, 2, "03-03-1992", "worker");
 
         Manager manager = new Manager("Bruce", 60000, 5, "05-05-1994", "manager");
 
-        ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(worker1);
-        employees.add(worker2);
-        employees.add(worker3);
-        employees.add(worker4);
-        employees.add(manager);
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-        for (Employee employee : employees){
-            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() + ", Position: " +
-                    employee.getPosition() + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary());
-        }
+        System.out.println("Worker2 (" + worker2.getName() + ") equals worker1 (" + worker1.getName() + "): " + worker2.equals(worker1));
+        System.out.println("Worker2 (" + worker2.getName() + ") equals worker3 (" + worker3.getName() + "): " + worker2.equals(worker3));
+        System.out.println("Worker2 (" + worker2.getName() + ") equals manager (" + manager.getName() + "): " + worker2.equals(manager));
 
     }
 }
